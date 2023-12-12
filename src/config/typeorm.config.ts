7203fc056c01693eadf9631a3640d22c4109b596
Agustin Prado <aprado@Agustins-MBP.fibertel.com.ar>
2023-12-11 22:00:18 -0300

    config for envs

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,6 +9,6 @@ export const TypeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
-  entities: ['dist/**/*.entity.js'],
+  entities: ['dist/**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize, //only for development
 };
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
+  const options = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  //app.use(cors(options))
+  app.enableCors(options);
+
   const port = process.env.PORT || serverConfig.port;
 
   await app.listen(port);
